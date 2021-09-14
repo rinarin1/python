c78249c5d24117e04ca5d1f29ceae95000ba9e69
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2021-09-14 11:45:51 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+import pyautogui
+pyautogui.click(1220, 750)
+pyautogui.click(1219, 672)
+pyautogui.click(100, 740)
+pyautogui.hotkey('ctrl', 't')
+pyautogui.click (161, 49)
+pyautogui.write('vk.com')
+pyautogui.press('enter')
+
