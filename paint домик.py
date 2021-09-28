1d85fb58e8e3c3948004fae3bfdfa83296277ea1
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2021-09-28 11:58:29 +0700

    Add files via upload

---
+++ paint домик.py
@@ -0,0 +1,21 @@
+import pyautogui as pg
+import time
+pg.click (268,737)
+time.sleep(2)
+pg.click (938,63)
+pg.click (554,65)
+time.sleep(1)
+pg.mouseDown (267,371)
+time.sleep(1)
+pg.mouseUp (436,526)
+pg.click (383,70) 
+pg.click (373,473)
+time.sleep(1)
+pg.click (1044,63)
+pg.click (490,81)
+pg.mouseDown (267,371)
+pg.mouseUp (439,264)
+pg.click (382,69)
+pg.click (354,329)
+
+
