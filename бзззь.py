72dca37d93e45b923820290b8968b78c01daf6ea
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2021-10-05 11:21:06 +0700

    Add files via upload

---
+++ бзззь.py
@@ -0,0 +1,6 @@
+chislo=input('введите число с дробное частью')
+chislo1=input('введите другое число с дробной частью')
+chislo=float(chislo)
+chislo1=float(chislo1)
+chislo2=chislo-chislo1
+print(chislo2)
