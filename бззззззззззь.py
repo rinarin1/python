9f20682a1a27c387823125cf8ac052a4a30efc1c
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2021-10-12 11:50:18 +0700

    Add files via upload

---
+++ бззззззззззь.py
@@ -0,0 +1,9 @@
+chislo=int(input('введите число'))
+if chislo%3==0:
+    print('делится на 3')
+else:
+    print('не делится на 3')
+if chislo>0:
+    print('больше 0')
+else:
+    print('не больше 0')
