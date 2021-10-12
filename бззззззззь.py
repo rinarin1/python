14ee10ea8c8b7ad061d2919033c4773a4fcf2175
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2021-10-12 11:32:24 +0700

    Add files via upload

---
+++ бззззззззь.py
@@ -0,0 +1,22 @@
+chislo=int(input('введите цифру от 0 до 9'))
+if chislo==0:
+    print('...')
+if chislo==1:
+    print('А')
+if chislo==2:
+    print('Б')
+if chislo==3:
+    print('В')
+if chislo==4:
+    print('Г')
+if chislo==5:
+    print('Д')
+if chislo==6:
+    print('Е')
+if chislo==7:
+    print('Ё')
+if chislo==8:
+    print('Ж')
+if chislo==9:
+    print('З')
+
