e1d3a8c8a48ae1a7060db5b53a54d46e0df75635
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2021-11-08 20:34:46 +0700

    Add files via upload

---
+++ ъ.py
@@ -0,0 +1,13 @@
+import pyautogui as pg
+import time as t
+k=int(input('Введите число:'))
+for step in range(1, k+1):
+    pg.hotkey('win', 'r')
+    pg.write('notepad')
+    pg.press('enter')
+    t.sleep(1)
+    pg.hotkey('ctrl', 's')
+    t.sleep(2)
+    pg.click(322, 432)
+    pg.write(step)
+    pg.press('enter')
