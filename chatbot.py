726014e4099edfc34efc2bb27020535fd2bd57fd
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2022-03-22 11:25:27 +0700

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,54 @@
+import pyautogui as pg
+import time
+def brauzer():
+    v=int(input('кол-во вкладок:'))
+    pg.hotkey ('ctrl', 'alt', 'm')
+    time.sleep(5)
+    for step in range (1, v):
+        pg.hotkey('ctrl', 't')
+def notepad():
+    pg.hotkey('win' , 'r')
+    time.sleep(1)
+    pg.hotkey('shift', 'ctrl')
+    pg.write ('notepad')
+    pg.press ('enter')
+def plus():
+    slagaemoe1=float(input('Введите первое слагаемое:'))
+    slagaemoe2=float(input('Введите второе слагаемое:'))
+    summa=slagaemoe1+slagaemoe2
+    print(summa)
+def minus():
+    umenshaemoe=float(input('Введите уменьшаемое:'))
+    vichitaemoe=float(input('Введите вычитаемое:'))
+    raznost=umenshaemoe-vichitaemoe
+    print(raznost)
+def zvezdochka():
+    mnozitel1=float(input('Введите первый множитель:'))
+    mnozitel2=float(input('Введите второй множитель:'))
+    proizvedenie=mnozitel1*mnozitel2
+    print(proizvedenie)
+def delenie():
+    delimoe=float(input('Введите делимое:'))
+    delitel=float(input('Введите делитель:'))
+    raznost=delimoe/delitel
+    print(raznost)
+while True:
+    z=input('запрос:').lower()
+    if z == 'браузер':
+        brauzer()
+    if z == 'блокнот':
+        notepad()
+    if z == 'калькулятор':
+        d=input('операция:')
+        while d!='+' and d!='-' and d!='*' and d!='/':
+            d=input('действие:')
+        if d == '+':
+            plus()
+        if d == '-':
+            minus()
+        if d =='*':
+            zvezdochka()
+        if d =='/':
+            delenie()
+    if z == 'выход':
+        pg.hotkey('alt', 'f4')
