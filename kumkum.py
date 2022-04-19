ff6e8740c8a3b4a284e4b02bee9fbfa89ea997d1
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2022-04-19 12:03:44 +0700

    Update kumkum.py

--- kumkum.py
+++ kumkum.py
@@ -1,18 +1,22 @@
 import random
-chislo1=random.randint(0,9)
-chislo2=random.randint(0,9)
-while chislo1==chislo2:
-    chislo2=random.randint(0,9)
-chislo3=random.randint(0,9)
-while chislo1==chislo3 or chislo2==chislo3:
-    chislo3=random.randint(0,9)
-chisla=chislo1, chislo2, chislo3
-chislo4=int(input('Введите первую цифру:'))
-chislo5=int(input('Введите вторую цифру:'))
-chislo6=int(input('Введите третью цифру:'))
-chisla1=chislo4, chislo5, chislo6
-if chisla==chisla1:
-    print('!')
+def proverka(per1, per2):
+    if per1==per2:
+        return True
+    else:
+        return False
+zivra1=random.randint(0,9)
+zivra2=random.randint(0,9)
+while zivra1==zivra2:
+    zivra2=random.randint(0,9)
+zivra3=random.randint(0,9)
+while zivra1==zivra3 or zivra2==zivra3:
+    zivra3=random.randint(0,9)
+chislo=str(zivra1)+str(zivra2)+str(zivra3)
+zivra4=input('Введите первую цифру:')
+zivra5=input('Введите вторую цифру:')
+zivra6=input('Введите третью цифру:')
+chislo1=zivra4+zivra5+zivra6
+if proverka(chislo, chislo1)==True:
+    print('+')
 else:
-    for chislo in chisla:
-        print(chislo)
+    print('-')
