5ffd0434c7f7b9315de240d27d2bd6b1541ab222
rinarin1 <90398376+rinarin1@users.noreply.github.com>
2022-04-26 10:48:28 +0700

    Add files via upload

--- kimkim.py
+++ kimkim.py
@@ -1,18 +1,21 @@
 import random
 schetchik=0
-chislo1=random.randint(0,9)
-chislo2=random.randint(0,9)
-while chislo1==chislo2:
-    chislo2=random.randint(0,9)
-chislo3=random.randint(0,9)
-while chislo1==chislo3 or chislo2==chislo3:
-    chislo3=random.randint(0,9)
-chislo4=int(input('Введите первую цифру:'))
-chislo5=int(input('Введите вторую цифру:'))
-chislo6=int(input('Введите третью цифру:'))
-if chislo1==chislo4:
-    schetchik=schetchik+1
-if chislo2==chislo5:
-    schetchik=schetchik+1
-if chislo3==chislo6:
-    schetchik=schetchik+1
+def proverka(per1, per2):
+    if per1==per2:
+        return True
+    else:
+        return False
+zivra1=random.randint(0,9)
+zivra2=random.randint(0,9)
+while zivra1==zivra2:
+    zivra2=random.randint(0,9)
+zivra3=random.randint(0,9)
+while zivra1==zivra3 or zivra2==zivra3:
+    zivra3=random.randint(0,9)
+chislo=str(zivra1)+str(zivra2)+str(zivra3)
+chislo1=input('Введите 3-ёх значное число:')
+for zivra in chislo:
+    for ziVra in chislo1:
+        if proverka(zivra, ziVra)==True:
+            schetchik=schetchik+1
+print(schetchik)
